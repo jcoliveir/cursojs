7ebead2d1f9a41fd7a1297dd990ca193806b7672
jcoliveir <jean-bes@hotmail.com>
2024-10-05 13:50:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * 1 - NUNCA INICIAR COM CARACTERE ESPECIAL, EXCETO O_ (UNDERLINE/UNDERSCORE)
+ * 2 - NUNCA INICIAR COM NÚMERO
+ * 3 - NUNCA SEPARAR PALAVRAS COMPOSTAS COM ESPAÇO
+ * 3a - Ao separar palavras, utilizar um dos 3 modelos
+ *     PascalCase - toda palavra é iniciada por letramaiscula
+ *     camelCase  - A partir da segunda palavra, iniciar em letraMaiúscula
+ *     snake_case - Separar as palavras por _ (UNDERLINE/UNDERSCORE) * 
+ * 
+ * const nome = 'jean';
+ * let numero = 10; // -10
+ * let numero_decimal = 3.14; // -3,14
+ * let booleana = true; // false
+ * let nulo = null // vazio
+ * let indefino = undefined // não definido
+ * 
+ * Operação matemática
+ *      SOMA -----------  +
+ *      SUBTRAÇÃO ------  -
+ *      MULTIPLICAÇÃO --  *
+ *      DIVISÃO --------  /
+ *      RESTO ----------  %
+ *      EXPONENCIAÇÃO --  **
+ */
+
+
+//const nome = 'jean';
+//const numero = 29;
+//const frase             = 'Olá, meu nome é '+ nome +' e tenho '+ idade +'anos';
+//const frase_interpolada = `Olá, meu nome é ${nome} e tenho ${idade} anos.`;
+
+//const num = 8;
+//let multiplicador = 1
+//.log(`${num} x ${multiplacador} = ${num*multiplicador}`)
+//console.log(++multiplicador);
+
+
+let n1 = 8
+let n2 = 7
+let n3 = 3
+let n4 = 5
+
+let media = (n1+n2+n3+n4) / 4;
+
+console.log(`A media é ${media}`)
+
+
+
+
